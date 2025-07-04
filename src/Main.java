28f141efb387e39cad4a0002a3f8e2a834292015
Mugunthan234 <mugunthanmugunthan839@gmail.com>
2025-07-04 19:51:40 +0530

    added upperCase to gender Variable

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         sc.nextLine();
         System.out.println("Enter Your Gender " + userName+" if age 1-8 (Children) or 6-12 Months (Baby) or 0-6 Months (Infant): ");
         String gender = sc.nextLine();
+        gender= Character.toUpperCase(gender.charAt(0)) + gender.substring(1);
 
         if(age>0){
         if(age>=60 && Objects.equals(gender, "Male")){
@@ -35,6 +36,8 @@ public class Main {
         } else{
             System.out.println("You Entered the Wrong Age.");
         }
-        
+
+
+
     }
 }
